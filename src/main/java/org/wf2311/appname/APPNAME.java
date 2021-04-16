33be5bf9fddc64680e86bb6e2a27a12f1a6d0828
wf2311 <wf2311@163.com>
2021-04-16 13:36:17 +0800

    refactor: 代码优化

--- src/main/java/org/wf2311/appname/APPNAME.java
+++ src/main/java/org/wf2311/appname/APPNAME.java
@@ -1,7 +1,7 @@
 package org.wf2311.appname;
 
-import com.wf2311.common.AfterServiceStartedRunner;
 import com.wf2311.common.utils.PropertiesUtil;
+import com.wf2311.spring.extend.AfterServiceStartedRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
  * @author <a href="mailto:wf2311@163.com">wf2311</a>
  * @since 2021/1/5 19:18.
  */
-@SpringBootApplication(scanBasePackages = {"org.wf2311.appname","com.wf2311.common.exception"})
+@SpringBootApplication(scanBasePackages = {"org.wf2311.appname","com.wf2311.spring.extend.exception"})
 @EnableDiscoveryClient
 public class APPNAME {
     @Bean
